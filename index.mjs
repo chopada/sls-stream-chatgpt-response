5deade8db8ffc2e31a3e271c0fa642166761f3b8
chopada <chopada.tushar@gmail.com>
2023-12-21 15:35:00 +0530

    Initial commit...

---
+++ index.mjs
@@ -0,0 +1,61 @@
+'use strict'
+
+import { streamOpenAIResponse } from './Lib/Helpers.mjs'
+
+export const handler = awslambda.streamifyResponse(
+  async (event, responseStream, _context) => {
+    // Get current process time
+    const startTime = process.hrtime()
+
+    // Start response stream
+    const metadata = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    }
+    responseStream = awslambda.HttpResponseStream.from(responseStream, metadata)
+    responseStream.write('\nRequest started.')
+    responseStream.write(`\n---\n`)
+
+    // Stream OpenAI response 1
+    const res_1 = await streamOpenAIResponse(responseStream, {
+      messages: [
+        { role: 'user', content: event.queryStringParameters.message }
+      ], // The user's message (in testing from event.json)
+      model: 'gpt-3.5-turbo',
+      temperature: 1,
+      max_tokens: 100
+    })
+
+    responseStream.write(`\n\n`)
+
+    // Stream OpenAI response 2
+    // const res_2 = await streamOpenAIResponse(responseStream, {
+    //   messages: [
+    //     {
+    //       role: 'user',
+    //       content: 'What is the meaning of this:\n\n' + res_1 + '\n\nAnswer: '
+    //     }
+    //   ],
+    //   model: 'gpt-3.5-turbo',
+    //   temperature: 0,
+    //   max_tokens: 200
+    // })
+
+    // ... add more responses as needed here.
+
+    // Return execution time (optional)
+    const hrTime = process.hrtime(startTime)
+    const totalSeconds = hrTime[0] + hrTime[1] / 1e9
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = (totalSeconds % 60).toFixed(2)
+    responseStream.write(`\n---\n`)
+    responseStream.write(
+      `Execution Time: ${minutes} minutes ${seconds} seconds\n`
+    )
+
+    // End response stream
+    responseStream.end()
+  }
+)
---
+++ test_online.sh
@@ -0,0 +1,1 @@
+curl -G --location "https://mz4xfaqz2xmxwkh26vtajkytve0cjurg.lambda-url.ap-south-1.on.aws" --data-urlencode "message=$1"
